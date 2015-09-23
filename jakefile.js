ac08a6a6d282225b47daacfd133742e74b0bf5b8
Helio Alves <helios25@gmail.com>
2015-09-22 21:29:04 -0400

    Build script expects Node Version from package.json

--- jakefile.js
+++ jakefile.js
@@ -1,19 +1,22 @@
 (function() {
     "use strict";
 
-    var EXPECTED_NODE_VERSION = "v4.0.0";
     desc("Default build");
     task("default", [ "version" ], function() {
         console.log("\n\nBUILD OK");
     });
-
     desc( "Checking Node Version");
+
     task("version", function() {
         console.log("Checking Node Version: .");
 
+        var packageJson = require("./package.json");
+
+        var expectedVersion = "v" + packageJson.engines.node;
+
         var actualVersion = process.version;
-        if (actualVersion !== EXPECTED_NODE_VERSION) {
-            fail( "Incorrect Node version: expected " + EXPECTED_NODE_VERSION + " but was " + actualVersion);
+        if (actualVersion !== expectedVersion) {
+            fail( "Incorrect Node version: expected " + expectedVersion + " but was " + actualVersion);
         }
     });
 }());
