e80b9dd12934c598eda7497ac382006d45eaf393
Helio Alves <helios25@gmail.com>
2015-09-22 21:15:14 -0400

    Build script to check node version

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,19 @@
 (function() {
     "use strict";
+
+    var EXPECTED_NODE_VERSION = "v4.0.0";
     desc("Default build");
-    task("default", function() {
+    task("default", [ "version" ], function() {
         console.log("\n\nBUILD OK");
+    });
+
+    desc( "Checking Node Version");
+    task("version", function() {
+        console.log("Checking Node Version: .");
+
+        var actualVersion = process.version;
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail( "Incorrect Node version: expected " + EXPECTED_NODE_VERSION + " but was " + actualVersion);
+        }
     });
 }());
