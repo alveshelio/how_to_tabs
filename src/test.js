bce7f5e5c3cc772f5d747017f5aef8a7dd999180
Helio Alves <helios25@gmail.com>
2015-09-24 15:15:10 -0400

    Example of a basic assertion

--- src/test.js
+++ src/test.js
@@ -1,4 +1,17 @@
 ( function () {
     "use strict";
 
+
+    /* assertion */
+    assertEqual(add(3, 4), 7);
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function assertEqual(actual, expected) {
+        if (actual !== expected) {
+            throw new Error("We expected " + expected + " and go " + actual);
+        }
+    }
 }());
