1da62f444d36d817b337c1a0ac29059fe7993e0c
Helio Alves <helios25@gmail.com>
2015-09-25 10:31:39 -0400

    Added Mocha to run our tests

--- src/test.js
+++ src/test.js
@@ -3,16 +3,23 @@
 
     var assert = require("chai").assert;
 
-    /* Basic addition */
-    assert.equal(add(3,4), 7, "Expected 7 but got " + add(3, 4));
+    //var mocha = require("mocha");
 
-    /* IEEE 754 floating point */
-    assert.equal(add(0.1 + 0.2), 0.3);
+    describe("Addition", function() {
+
+        it("adds positive numbers", function() {
+            /* Basic addition */
+            assert.equal(add(3,4), 7, "Expected 7 but got " + add(3, 4));
+        });
+
+        it("uses IEEE 754 floating point", function() {
+            assert.equal(add(0.1, 0.2), 0.30000000000000004);
+        });
+
+    });
 
     function add(a, b) {
-        return a - b;
+        return a + b;
     }
-
-
 
 }());
