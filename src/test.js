2ccf879d1dff67f0f29e523000c389f0c70464f6
Helio Alves <helios25@gmail.com>
2015-09-25 09:30:53 -0400

    Added a basic assertion

--- src/test.js
+++ src/test.js
@@ -2,5 +2,16 @@
     "use strict";
 
 
+    assertEqual( add(3, 4), 7);
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function assertEqual(actual, expected) {
+        if (actual !== expected) {
+            throw new Error("Expected: " + expected + " and got: " + actual);
+        }
+    }
 
 }());
