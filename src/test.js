9a3a5241c267cd08ed3c5531a8f053702bb25e70
Helio Alves <helios25@gmail.com>
2015-09-25 13:10:43 -0400

    Installed Jasmine and modified test to work with Jasmine

--- src/test.js
+++ src/test.js
@@ -9,14 +9,22 @@
 
         it("adds positive numbers", function() {
             /* Basic addition */
-            assert.equal(add(3,4), 7, "Expected 7 but got " + add(3, 4));
+            //assert.equal(add(3,4), 7, "Expected 7 but got " + add(3, 4));
+            expect(add(3,4)).toBe(7);
         });
 
         it("uses IEEE 754 floating point", function() {
-            assert.equal(add(0.1, 0.2), 0.30000000000000004);
+            //assert.equal(add(0.1, 0.2), 0.30000000000000004);
+            expect(add(0.1, 0.2)).toBe(0.30000000000000004);
         });
 
     });
+
+    //describe("A Jasmine 1.3 spec", function() {
+    //    it("should have toBe matcher", function() {
+    //        expect(1).toBe(1);
+    //    });
+    //});
 
     function add(a, b) {
         return a + b;
