161b97b63df8bade8577d9dd97f70179f81f4206
Helio Alves <helios25@gmail.com>
2015-09-25 10:21:19 -0400

    Added an example with floating numbers

--- src/test.js
+++ src/test.js
@@ -3,7 +3,11 @@
 
     var assert = require("chai").assert;
 
+    /* Basic addition */
     assert.equal(add(3,4), 7, "Expected 7 but got " + add(3, 4));
+
+    /* IEEE 754 floating point */
+    assert.equal(add(0.1 + 0.2), 0.3);
 
     function add(a, b) {
         return a - b;
