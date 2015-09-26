9577b1fc0758f278062eb9a72d6858178a114039
Helio Alves <helios25@gmail.com>
2015-09-25 20:46:49 -0400

    Installed and removed testem

--- src/test.js
+++ src/test.js
@@ -1,20 +1,28 @@
 ( function () {
     "use strict";
 
-    var assert = require("chai").assert;
+    //var assert = {
+    //
+    //};
 
     describe("Addition", function() {
 
         it("adds positive numbers", function() {
             /* Basic addition */
-            assert.equal(add(3,4), 7);
+            assertEqual(add(3,4), 7);
             //expect(add(3,4)).toBe(7);
         });
 
         it("uses IEEE 754 floating point", function() {
-            assert.equal(add(0.1, 0.2), 0.30000000000000004);
+            assertEqual(add(0.1, 0.2), 0.30000000000000004);
             //expect(add(0.1, 0.2)).toBe(0.30000000000000004);
         });
+
+        function assertEqual(actual,expected) {
+            if (actual !== expected) {
+                throw new Error("expected" + expected + " and got " + actual);
+            }
+        }
 
     });
 
