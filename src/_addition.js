318b0cbc699dc831e96ce03f61c36d78a505c442
Helio Alves <helios25@gmail.com>
2015-09-27 10:22:16 -0400

    Refactored our example test into multiple modules

--- src/_addition.js
+++ src/_addition.js
@@ -1,20 +1,18 @@
 ( function () {
     "use strict";
 
-    //var assert = {
-    //
-    //};
+    var addition = require("./addition.js");
 
     describe("Addition", function() {
 
         it("adds positive numbers", function() {
             /* Basic addition */
-            assertEqual(add(3,4), 7);
+            assertEqual(addition.add(3,4), 7);
             //expect(add(3,4)).toBe(7);
         });
 
         it("uses IEEE 754 floating point", function() {
-            assertEqual(add(0.1, 0.2), 0.30000000000000004);
+            assertEqual(addition.add(0.1, 0.2), 0.30000000000000004);
             //expect(add(0.1, 0.2)).toBe(0.30000000000000004);
         });
 
@@ -25,9 +23,5 @@
         }
 
     });
-
-    function add(a, b) {
-        return a + b;
-    }
 
 }());
---
+++ src/addition.js
@@ -0,0 +1,11 @@
+( function () {
+    "use strict";
+
+    // exports.nameOfFunction makes the function
+    // available to any file that requires this file
+    exports.add = function add(a, b) {
+        return a + b;
+    }
+
+
+}());
